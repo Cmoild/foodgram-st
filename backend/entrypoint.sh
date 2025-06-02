43ead4023717aca5405a9ae4b9d0ee4667d2de9b
Nikita <cold17748@gmail.com>
2025-06-02 18:02:21 +0300

    correction

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-
+# Ингредиенты заргужаются в БД после применения миграций
+# Это происходит по сигналу post_migrate
+# Код для загрузки находится в recipes_models/apps.py
+# Данные загружаются только в том случае, если таблица пуста
 python manage.py migrate
 python manage.py collectstatic --noinput
 gunicorn --bind 0.0.0.0:8000 backend.wsgi
